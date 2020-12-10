1c9c4eeb5f6b4d52bb197eb314b0d613c756b040
brynknowles <bryn.knowles@gmail.com>
2020-12-10 15:25:01 -0500

    done

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Side from '../components/Side'
 
 class Order extends Component {
   state = {
@@ -6,7 +7,11 @@ class Order extends Component {
   }
 
   handleClick = () => {
-    this.state.isClicked = !this.state.isClicked
+    this.setState( prevState => {
+      return {
+        isClicked: !prevState.isClicked
+      }
+    })
   }
 
   render() {
@@ -36,7 +41,7 @@ class Order extends Component {
           }
 
           { /* this is just a shortcut to writing this.state.isClicked ? <Side sides={this.props.sides} /> : null */ }
-          { this.state.isClicked && <Side sides={this.props.sides} /> }
+          { this.state.isClicked && <Side sides={ this.props.sides } /> }
 
         </div>
       </div>
